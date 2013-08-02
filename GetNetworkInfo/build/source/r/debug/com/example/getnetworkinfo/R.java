10967b66204436f8444e2e5656c27e3429490ed4
hojun baek <mr.hjbaek@gmail.com>
2013-08-02 13:50:57 -0600

    Structured some UI-Components

--- GetNetworkInfo/build/source/r/debug/com/example/getnetworkinfo/R.java
+++ GetNetworkInfo/build/source/r/debug/com/example/getnetworkinfo/R.java
@@ -21,7 +21,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080004;
+        public static final int ipAddressTv=0x7f080003;
+        public static final int navigation=0x7f080000;
+        public static final int setupBtn=0x7f080001;
+        public static final int sharedBtn=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- GetNetworkInfo/src/main/java/com/example/getnetworkinfo/MainActivity.java
+++ GetNetworkInfo/src/main/java/com/example/getnetworkinfo/MainActivity.java
@@ -1,23 +1,84 @@
 package com.example.getnetworkinfo;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.getnetworkinfo.util.NetworkUtils;
 
 public class MainActivity extends Activity {
 
+    private boolean _isRunningThread = false;
+
+    private TextView _ipAddressTv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        _ipAddressTv = (TextView)findViewById(R.id.ipAddressTv);
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Check Network whether Wifi is connected or else.
+        // If Network is connected, it gets network information.
+        //
+        if (_isRunningThread == false) {
+            _isRunningThread = true;
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    if (_isRunningThread == false) return;
+
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    //
+                    // Update UI
+                    //
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            _ipAddressTv.setText("IP Address : " + NetworkUtils.getIPAddress(true));
+                        }
+                    });
+                }
+            }).start();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        _isRunningThread = false;
+    }
+
+    @Override
+    public void onBackPressed() {
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        // Not used.
+        return false;
     }
-    
+
+    /**
+     * It calls when you click any button.
+     * @param v
+     */
+    public void onAction(View v) {
+        switch (v.getId()) {
+            case R.id.setupBtn:
+                return;
+            case R.id.sharedBtn:
+                return;
+        }
+    }
 }
